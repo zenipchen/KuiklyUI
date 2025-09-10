52ca59d876a490d721091af1123a3155c0d45c26
zenipchen <zhenhuachen@tencent.com>
2025-09-10 18:01:38 +0800

    fix: 迁移无障碍

--- compose/src/commonMain/kotlin/com/tencent/kuikly/compose/ComposeContainer.kt
+++ compose/src/commonMain/kotlin/com/tencent/kuikly/compose/ComposeContainer.kt
@@ -240,6 +240,10 @@ open class ComposeContainer :
         return pageData.isAccessibilityRunning
     }
 
+    override fun isAccessibilityRunning(): Boolean {
+        return pageData.isAccessibilityRunning
+    }
+
     /**
      * 注册扩展module接口，（注：注册时机为override ComponentActivity.createExternalModules中统一注册）
      */
