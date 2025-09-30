b2c3a387d09920dce7842b9dadbe1c81621687b6
zenipchen <zhenhuachen@tencent.com>
2025-09-30 17:05:52 +0800

    feat: sync send onBackPressed event on android

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -200,3 +200,39 @@ kuikly {
         // addSplitPage("route","home")
     }
 }
+
+
+//project.tasks.register("hotPreviewDetectClasspath") {
+//    group = "hotPreview"
+//    val targetName = try {
+//        val kotlinExt = project.extensions.findByName("kotlin")
+//        val targets = kotlinExt?.javaClass?.getMethod("getTargets")?.invoke(kotlinExt) as? Collection<*>
+//        targets?.forEach { println(it?.javaClass?.simpleName) }
+//        val jvmTarget = targets?.firstOrNull { it?.javaClass?.simpleName?.contains("KotlinAndroidTarget") ?: false }
+//        jvmTarget?.javaClass?.getMethod("getName")?.invoke(jvmTarget) as? String ?: "jvm"
+//    } catch (err: Throwable) {
+//        "jvm"
+//    }
+//    val runtimeClasspath = project.configurations
+//        .findByName("${targetName}DebugRuntimeClasspath")?.incoming?.artifactView {
+//            this.isLenient = true
+//        }?.artifacts?.map { it.file.absolutePath }
+//    val compileClasspath = project.configurations
+//    .findByName("${targetName}DebugCompileClasspath")?.incoming?.artifactView {
+//        this.isLenient = true
+//    }?.artifacts?.map { it.file.absolutePath }
+//    //val allConfigurationNames = project.configurations.joinToString("\n") { it.name }
+//
+//    val runtimeFile = layout.buildDirectory.file("hotPreviewRuntimeClasspath.txt")
+//    val compileFile = layout.buildDirectory.file("hotPreviewCompileClasspath.txt")
+//    //val configFile = layout.buildDirectory.file("hotPreviewConfigurations.txt")
+//    val jvmTargetName = layout.buildDirectory.file("hotPreviewJvmTargetName.txt")
+//    val buildDirectory = layout.buildDirectory
+//    doLast {
+//        buildDirectory.get().asFile.mkdirs()
+//        //configFile.get().asFile.writeText(allConfigurationNames)
+//        runtimeFile.get().asFile.writeText(runtimeClasspath?.joinToString("\n") ?: "")
+//        compileFile.get().asFile.writeText(compileClasspath?.joinToString("\n") ?: "")
+//        jvmTargetName.get().asFile.writeText(targetName)
+//    }
+//}
