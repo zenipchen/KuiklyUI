f620998af89ab4dca28fef396534cc22e40a3ee3
zenipchen <zhenhuachen@tencent.com>
2026-01-04 15:52:02 +0800

    fix: find page info correct

--- PreviewMacApp/PreviewMacApp/KuiklyRenderAddons/Controllers/KuiklyContainerManager.m
+++ PreviewMacApp/PreviewMacApp/KuiklyRenderAddons/Controllers/KuiklyContainerManager.m
@@ -454,7 +454,7 @@
 - (NSDictionary<NSString *, id> *)mergeExtendedParametersWithOriginalParameters:(nullable NSDictionary<NSString *, id> *)parameters {
     NSMutableDictionary<NSString *, id> *mergedParameters = [parameters ?: @{} mutableCopy];
     mergedParameters[@"_preview_mode"] = @YES;
-    mergedParameters[@"inspectionMode"] = @0;
+    mergedParameters[@"inspectionMode"] = @1;
     return [mergedParameters copy];
 }
 
